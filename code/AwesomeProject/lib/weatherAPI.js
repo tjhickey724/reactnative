03ab0c42010bb299d801c8e929e422f69fed86ef
tim <tjhickey@brandeis.edu>
2023-06-14 11:14:33 -0400

    added weatherAPI.js

--- code/AwesomeProject/lib/weatherAPI.js
+++ code/AwesomeProject/lib/weatherAPI.js
@@ -2,6 +2,8 @@ const axios = require('axios')
 
 // returns a list of matches for an address
 // if there is a match the latlon is in the coordinates field
+// URL is
+// https://geocoding.geo.census.gov/geocoder/locations/onelineaddress?address=ADDRESS&benchmark=2020&format=json
 const get_latlon = async (address) => {
     address = encodeURI(address);
     let url="https://geocoding.geo.census.gov/geocoder"+
@@ -33,4 +35,4 @@ const get_weather = async (address) => {
   }
 }
 
-export {get_latlon, get_forecastURL, get_forecast, get_weather}
+export {get_latlon, get_forecastURL,  get_weather}
