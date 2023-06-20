39368cb2110bb941cfed2ee179e56fdb1c737242
tim <tjhickey@brandeis.edu>
2023-06-20 11:08:22 -0400

    added StickerSmash demo

--- code/StickerSmash/App.js
+++ code/StickerSmash/App.js
@@ -1,15 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, Image, View } from 'react-native';
-
+import ImageViewer from './components/ImageViewer';
+import Button from './components/Button';
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={PlaceholderImage} style={styles.image} />
+        <ImageViewer placeholderImageSource={PlaceholderImage} /> 
       </View>
-      <Text style={{ color: '#fff' }}>Open up App.js to start working on your app!</Text>
+      <View style={styles.footerContainer}>
+        <Button label="Choose a photo" />
+        <Button label="Use this photo" />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -25,10 +29,10 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingTop: 58,
   },
-  image: {
-    width: 320,
-    height: 440,
-    borderRadius: 18,
+  footerContainer: {
+    flex: 1 / 3,
+    alignItems: 'center',
   },
+
 });
 
