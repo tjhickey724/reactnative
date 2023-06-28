d4d9f54c4aa6303d9fae11fecd4f189b2853435e
tim <tjhickey@brandeis.edu>
2023-06-28 19:23:22 -0400

    added views back

--- code/gameserver/app.js
+++ code/gameserver/app.js
@@ -30,8 +30,9 @@ const Room =
 var app = express();
 
 // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
