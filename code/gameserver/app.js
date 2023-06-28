0a20774ef976e6e842883dbbbf5fcad535d2b237
tim <tjhickey@brandeis.edu>
2023-06-28 19:20:21 -0400

    minor changes

--- code/gameserver/app.js
+++ code/gameserver/app.js
@@ -88,8 +88,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  //res.render('error');
-  res.json(err);
+  res.render('error');
 });
 
 module.exports = app;
