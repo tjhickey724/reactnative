4417e0fa8cd3b3e4f01aa37bfccdf0db01bc6506
tim <tjhickey@brandeis.edu>
2024-06-26 08:57:47 -0400

    updated wordlewithfriends

--- code/gameserver_noDB/app.js
+++ code/gameserver_noDB/app.js
@@ -4,19 +4,20 @@ This is a minimal app to allow phone apps to communicate with each other.
 The goal of this app was to build the simplest possible app that would meet that goal.
 */
 
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-
-var app = express();
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -27,13 +28,25 @@ app.get("/", (req,res,next) => {
   res.json("Game Server "+Date());
 })
 
-let rooms ={'152a':{a:5,b:6},'test':{a:1,b:2}};
+let rooms ={cs153aSum24:{tim:5,rotem:7}};
 
 app.get("/room", (req,res,next) => {
   const id = req.query.id;
-  res.json(rooms[id]);
+  console.log(`in app.get(/room) with id=${id}`)
+  console.log(`and rooms[id] = ${JSON.stringify(rooms[id])}`)
+  if (id in rooms) {
+    res.json(rooms[id]);
+  }else {
+    res.json({});
+  }
 })
 
+/*
+ You can encode any JSON object x as a string with 
+ s = JSON.stringify(x)
+ and you can parse it from a string s to an object with
+ x = JSON.parse(s)
+*/
 app.post("/room", (req,res,next) => {
   console.log('handling post request to room')
   console.dir(req.body)
